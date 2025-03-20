19ca49ab8eb616ede8f593b2a7162aff4e1c0ba5
HAM <hamiosn@qq.com>
2025-03-20 20:40:31 +0800

    feat(renderer): 添加 VueFlow 组件及相关功能
    - 新增 VueFlow 组件，用于展示和操作流程图
    - 添加 initial-elements.ts 文件，定义初始节点和边
    - 引入 @vue-flow/core 依赖，支持流程图的核心功能
    - 更新 App.vue，调整布局并集成 VueFlow 组件
    - 新增 main-back.css 和 main.css，优化样式和布局

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,15 +1,14 @@
 <script setup lang="ts">
 import Versions from './components/Versions.vue'
 import Draggable from './components/Draggable.vue'
+import VueFlow from './components/VueFlow.vue';
 
 const ipcHandle = () => window.electron.ipcRenderer.send('ping')
 </script>
 
 <template>
-  <img alt="logo" class="logo" src="./assets/ison7-logo.svg" />
-  <div class="creator">Powered by HAM</div>
-  <p class="tip">Please try pressing <code>F12</code> to open the devTool</p>
   <div class="actions">
+    <img alt="logo" class="logo" src="./assets/ison7-logo.svg" />
     <div class="action">
       <a href="https://ham-ison.github.io/" target="_blank" rel="noreferrer">HAM's Notes</a>
     </div>
@@ -18,5 +17,8 @@ const ipcHandle = () => window.electron.ipcRenderer.send('ping')
     </div>
   </div>
   <Draggable />
+  <VueFlow />
   <Versions />
+  <div class="creator">Powered by HAM</div>
+  <p class="tip">Please try pressing <code>F12</code> to open the devTool</p>
 </template>
